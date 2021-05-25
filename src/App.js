6632617302ce3f249260aab951ee824242c56747
Mohammed-Alramahi <vittosc1997@gmail.com>
2021-05-25 22:01:39 +0300

    lab-07

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Map from './component/Map';
+import Weather from './component/Weather';
 function App() {
   return (
     <div className="App">
-      <Map/>
+      <Map />
     </div>
   );
 }
