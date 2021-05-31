4299484ce7db8cfa2a0cfae809b1765d74a0edbd
Mohammed-Alramahi <vittosc1997@gmail.com>
2021-05-31 20:21:06 +0300

    lab 10

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -4,7 +4,9 @@ class Movie extends React.Component {
     render() {
         return (
             <div style={{ marginBottom: 50 }}>
-                <div className="text-primary"><h6>title: {this.props.title}</h6><p>description: {this.props.overview}</p>
+                <div className="text-primary"><h6>title: {this.props.title}</h6>
+                <img src={this.props.image} width="350" height="300" alt={this.props.title}/>
+                <p>description: {this.props.overview}</p>
                     <h6>release date: {this.props.date}</h6>
                 </div>
             </div>
